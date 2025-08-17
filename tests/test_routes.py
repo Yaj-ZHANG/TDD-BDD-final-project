4dbba56cc79653efb602c9e25dc325679b5b682f
yzhang <yajie.zhang00@gmail.com>
2025-08-17 18:32:00 -0400

    Updated test_routes.py by adding test case for update_product

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -181,6 +181,20 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
         self.assertIn("was not found", data["message"])
 
+    def test_update_product(self):
+        """It should Update an existing product"""
+        # create and post product to update
+        test_product = ProductFactory()
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # update
+        new_product = response.get_json()
+        new_product["description"] = "unknown"
+        response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_product = response.get_json()
+        self.assertEqual(updated_product["description"], "unknown")
+
     ######################################################################
     # Utility functions
     ######################################################################

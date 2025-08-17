f71d209fa1d5e090001c4212a02d1cd134218091
yzhang <yajie.zhang00@gmail.com>
2025-08-17 16:51:31 -0400

    Updated factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -51,7 +51,7 @@ class ProductFactory(factory.Factory):
     price = FuzzyDecimal(0.5, 2000.0, 2)
     available = FuzzyChoice(choices[True, False])
     category = FuzzyChoice(
-        [
+        choices=[
             Category.UNKNOWN,
             Category.CLOTHS,
             Category.FOOD,

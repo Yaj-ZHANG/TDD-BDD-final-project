1882549c70cc209574e3f63dd99cfe1f4c584f9d
yzhang <yajie.zhang00@gmail.com>
2025-08-17 17:50:06 -0400

    Updated routes.py by correcting a typo in  READ PRODUCT function

--- service/routes.py
+++ service/routes.py
@@ -116,7 +116,7 @@ def get_products(product_id):
     if not product:
         abort(status.HTTP_404_NOT_FOUND, f"Product with id'{product_id}' was not found.")
 
-    app.logger.info("Returning product: %s", product_name)
+    app.logger.info("Returning product: %s", product.name)
     return product.serialize(), status.HTTP_200_OK
 
 #

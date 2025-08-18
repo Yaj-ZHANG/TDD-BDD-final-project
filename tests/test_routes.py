6c3b1a82d6917c509b5cfe3af31dbf4c71402560
yzhang <yajie.zhang00@gmail.com>
2025-08-18 14:18:01 -0400

    Updated test_routes.py by adding test case test_delete_product

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -200,10 +200,10 @@ class TestProductRoutes(TestCase):
         products = self._create_products(5)
         product_count = self.get_product_count()
         test_product = products[0]
-        response = self.client.delete (f"{BASE_URL}/{test_product.id}")
+        response = self.client.delete(f"{BASE_URL}/{test_product.id}")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(len(response.data), 0)
-        response = self.client.get (f"{BASE_URL}/{test_product.id}")
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         new_count = self.get_product_count()
         self.assertEqual(new_count, product_count - 1)
